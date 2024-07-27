f973d7a8e0dae348e9bd146b00dc7e4e68226f95
Jaxilian <jax.carls@gmail.com>
2024-07-27 15:05:05 +0200

    update

--- project.lua
+++ project.lua
@@ -1,11 +1,23 @@
 local module = {}
 module.TYPES = {
-    APP     = "APP",
-    LIB     = "LIB",
-    SHARED  = "SHARED"
+    APP         = "APP",
+    LIB         = "LIB",
+    SHARED      = "SHARED"
 }
 
 module.Cache = {}
+
+local function get_os_root()
+    local os_target = os.target()
+    
+    if os_target == "windows" then
+        return "C:\\"
+    elseif os_target == "linux" or os_target == "macosx" then
+        return "/"
+    else
+        error("Unsupported OS: " .. os_target)
+    end
+end
 
 local function filter_debug(name)
     filter "configurations:debug"
@@ -16,20 +28,38 @@ local function filter_debug(name)
 
     defines { "_DEBUG" }
     symbols "On"
-    buildoptions { "-g -O0" }
+    
+    if os.target() == "windows" then
+        buildoptions { "-Od" } 
+    else
+        buildoptions { "-g -O0" }
+    end
 end
 
 local function filter_release(name) 
     filter "configurations:release"
 
-    if module.Cache[name].Type == module.TYPES.APP then
-        kind "WindowedApp"
+    if module.Cache[name].Type == module.TYPES.APP  then
+        if module.Cache[name].Console then
+            kind "ConsoleApp"
+        else
+            defines("_CONSOLE_HIDDEN")
+            kind "WindowedApp"
+        end
     end
     
     defines { "_NDEBUG" }
     optimize "On"
-    buildoptions { "-O3", "-flto", "-ftree-vectorize", "-finline-functions" }
-    linkoptions { "-flto" }
+    symbols "Off"
+
+    if os.target() == "windows" then
+        buildoptions { "/O2" } 
+        linkoptions { "/LTCG" }
+    else
+        buildoptions { "-O3", "-flto", "-ftree-vectorize", "-finline-functions" }
+        linkoptions { "-flto" }
+    end
+
 end
 
 local function set_defines(name)
@@ -54,17 +84,36 @@ local function set_common(name)
     filter_release(name)
 end
 
-function module.Begin(name, enableCPP, TYPE)
+
+local function does_exist(name)
+    local folders = os.matchdirs(os.getcwd() .. "/*")
+    for _, folder in ipairs(folders) do
+        local dirname = path.getname(folder)
+        if dirname == name then
+            return true
+        end
+    end
+
+    return false
+end
+
+function module.Begin(name, enableCPP, TYPE, showConsole)
+
+    if not does_exist(name) then
+        error("project \"" .. name .. "\" does not exist! Directory missing ./"..name)
+        return
+    end
 
     if module.Cache[name] then
-        print(name .. " already exist in cache, dublication?")
+        error(name .. " already exist in cache, dublication?")
         return
     end
 
     module.Cache[name] = {
         Name    = name,
         Cpp     = enableCPP,
-        Type    = TYPE
+        Type    = TYPE,
+        Console = showConsole
     }
 
     project(name)
@@ -74,10 +123,50 @@ function module.Begin(name, enableCPP, TYPE)
     else
         language("C")
     end
+
+    if TYPE == module.TYPES.LIB then
+        kind("StaticLib")
+        targetdir(get_os_root() .. "Development/static")
+    elseif TYPE == module.TYPES.SHARED then
+        kind("SharedLib")
+        targetdir(name .. "/bin/%{cfg.buildcfg}")
+    else
+        targetdir(name .. "/bin/%{cfg.buildcfg}")
+    end
+
+    debugdir(name .. "/bin/%{cfg.buildcfg}")
+    targetdir(name .. "/bin/%{cfg.buildcfg}")
+    objdir(name .. "/obj/%{cfg.buildcfg}")
+    location(name)
+
+    files {
+        name .. "/src/**.h", name .. "/src/**.c",
+        name .. "/src/**.hpp", name .. "/src/**.cc",
+        name .. "/src/**.cpp"
+    }
+    includedirs {
+        name .. "/src",
+        get_os_root() .. "Development/include/"
+    }
+    libdirs(
+        get_os_root() .. "Development/static"
+    )
 end
-   
+
+function module.Link(libName)
+    links{libName}
+end
+
+function AddDefinition(definition)
+    defines{definition}
+end
 
 function module.End(name)
+    if not module.Cache[name] then
+        error(name .. " project has not been started")
+        return
+    end
+
     set_common(name)
 end
 
--- workspace.lua
+++ workspace.lua
@@ -1,7 +1,12 @@
 local module = {}
 
 
-function module.New(name, start_project)
+
+function module.New(start_project)
+
+    local cwd = os.getcwd()
+    local name = path.getname(cwd)
+
     workspace(name)
     configurations { "debug", "release" }
     startproject(start_project)
